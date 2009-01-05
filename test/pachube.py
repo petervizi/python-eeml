207ef35b9c0dd64851249b83d91b5e0506c5ebbd
petervizi <peter.vizi@gmail.com>
2009-01-04 20:09:35 -0700

    Removing key information.

--- test/pachube.py
+++ test/pachube.py
@@ -4,7 +4,7 @@ import httplib
 import random
 
 url = '/api/1275.xml'
-key = 'f2a9bd5a63e6a397629ede7e44c80f6a0941ddecf9a986fed40aae95dab5392d'
+key = 'PACHUBE_API_KEY'
 
 pa = Pachube(url, key)
 pa.update([Data(1, random.random(), unit=RH()), Data(0, random.random(), unit=Celsius())])
