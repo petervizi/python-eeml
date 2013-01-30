acdaad69257ca780476e16cebdf2b7eeba550aea
dazzag24 <dazzag@gmail.com>
2013-01-30 04:29:28 -0800

    Added a new example
    Added a new example that demonstrates how to catch exceptions thrown from put()

---
+++ example/exception_example.py
@@ -0,0 +1,28 @@
+import eeml
+import serial
+from eeml import CosmError
+
+# parameters
+API_KEY = 'YOUR PERSONAL API KEY'
+FEED = 'YOUR PERSONAL FEED ID'
+API_URL = '/v2/feeds/{feednum}.xml' .format(feednum = FEED)
+
+serial = serial.Serial('/dev/ttyUSB0', 9600)
+readings = serial.readline().strip().split(' ') # the readings are separated by spaces
+
+# open up your cosm feed
+pac = eeml.Cosm(API_URL, API_KEY)
+
+# prepare the emml payload
+pac.update([eeml.Data(0, readings[0], unit=eeml.Celsius()), eeml.Data(1, readings[1], unit=eeml.RH())])
+
+# attempt to send the data to Cosm.  Attempt to handle exceptions, such that the script continues running.
+# You could optionally place some retry logic around the pac.put() command.
+try:
+	pac.put()
+except CosmError, e:
+	print('ERROR: pac.put(): {}'.format(e))
+except StandardError:
+	print('ERROR: StandardError')
+except:
+	print('ERROR: Unexpected error: %s' % sys.exc_info()[0])
